26c5815ac3c6415bc30d192391e8fe41fa243a12
Isabella Ramos <145922868+Iswb04@users.noreply.github.com>
2026-04-28 18:41:00 -0300

    280426

--- npc.py
+++ npc.py
@@ -1,10 +1,6 @@
 import ollama
 
 def falar_com_npc(pergunta):
-    """
-    Comunica com o modelo Llama3 via Ollama.
-    Retorna a resposta do NPC como string.
-    """
     try:
         resposta = ollama.chat(
             model="llama3:latest",
@@ -15,12 +11,12 @@ def falar_com_npc(pergunta):
                     VOCÊ É: Dono da taverna em Pomar Branco, ranzinza e exausto pela guerra.
 
                     COMPORTAMENTO:
-                    1. INICIAL: Trate o usuário como 'viajante'. Seja seco e tente vender bebida ou pouso.
+                    1. INICIAL: Trate o usuário como 'viajante'. Seja direto porém amigavél (só mude de comportamento após revelação ou suspeita)
                     2. REVELAÇÃO: Se ele citar 'Bruxo', 'monstros' ou 'magia', mude para modo HOSTIL.
                     3. PÓS-REVELAÇÃO: Caso o usuário se revelar como bruxo, chame-o de 'mutante'. Seja ríspido, pois bruxos trazem azar, não têm alma e lucram com a miséria alheia.
                     4. ESTILO: Português rústico, respostas curtas e diretas.
                     5. ANTI-MODERNO: Se falarem de tecnologia, diga que estão bêbados de hidromel.
-                    6. REMOVA explicações longas. Seja breve.
+                    6. REMOVA explicações longas. Seja breve. (um parágrafo)
                     7. Ficar com comportamenteo hostil quando o usuário citar uma mulher de cabelo escuro, olhos violeta e cheiro de lilás e groselha (ela é uma bruxa chamada Yennefer).
                     """
                     
@@ -37,3 +33,22 @@ def falar_com_npc(pergunta):
         return resposta['message']['content']
     except Exception as e:
         return f"Pelos deuses... a guerra destruiu tudo, até a conexão. (Erro: {e})"
+    
+
+
+
+    # em caso de npc.py
+"""if __name__ == "__main__":
+    print("\n\033[31m--- Entrando na Taverna de Pomar Branco ---\033[31m")
+    print("\n\033[33mTaverneiro:\033[0m Bem vindo a taberna de White Orchard. Se busca encrenca, veio ao lugar certo... se busca bebida, pague primeiro.")
+
+
+while True:
+        entrada_usuario = input("\n\033[33mVocê: \033[0m")
+        
+        if entrada_usuario.lower() in ["sair", "exit", "quit"]:
+            print("\033[33mTaverneiro:\033[33m Já vai tarde. Não esqueça de fechar a porta!")
+            break
+            
+        resposta_npc = falar_com_npc(entrada_usuario)
+        print(f"\n\033[33mTaverneiro: \033[0m {resposta_npc}")"""
